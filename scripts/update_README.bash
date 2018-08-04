5e537532b9f4882173e5619364b84eff7662c025
Jumpei Matsuda <jmatsu.drm@gmail.com>
2018-08-04 19:50:25 +0900

    Added a license

--- scripts/update_README.bash
+++ scripts/update_README.bash
@@ -53,12 +53,16 @@ cat<<'EOF'
 ```
 go get github.com/jmatsu/dpg
 ```
+
+## LICENSE
+
+Under MIT License. See [LICENSE](./LICENSE)
 EOF
 }
 
 if [[ $(git log --merges --format='%s' -1|awk '$0=$NF') =~ ^update_doc_on_.* ]]; then
-echo "Merged from doc update branch."
-return 0
+    echo "Merged from doc update branch."
+    return 0
 fi
 
 create_helps
