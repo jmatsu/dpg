a716cb74be9c6a6e791801ee22b9706f1af2adc1
Jumpei Matsuda <jmatsu.drm@gmail.com>
2018-08-21 02:54:09 +0900

    Revert adding email option

--- command/organizations/teams/members/package_option.go
+++ command/organizations/teams/members/package_option.go
@@ -15,12 +15,15 @@ type packageOption int
 
 const (
 	userName packageOption = iota
+	userEmail
 )
 
 func (o packageOption) name() string {
 	switch o {
 	case userName:
 		return constant.UserName
+	case userEmail:
+		return constant.UserEmail
 	}
 
 	panic("Option name mapping is not found")
@@ -33,6 +36,11 @@ func (o packageOption) flag() cli.Flag {
 			Name:  o.name(),
 			Usage: "The name of the organization's user",
 		}
+	case userEmail:
+		return &cli.StringFlag{
+			Name:  o.name(),
+			Usage: "The e-mail address of the organization's user",
+		}
 	}
 
 	panic("Option name mapping is not found")
@@ -44,6 +52,7 @@ func addFlags() []cli.Flag {
 		organizations.OrganizationName.Flag(),
 		teams.TeamName.Flag(),
 		userName.flag(),
+		userEmail.flag(),
 	}
 }
 
